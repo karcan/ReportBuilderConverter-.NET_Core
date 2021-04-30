056cb201788617cb232b450d0cc05f076e5460f8
Karcan Ozbal <karcanozbal@hotmail.com.tr>
2021-04-30 15:56:52 +0300

    Add project files.

---
+++ Business/IReportConverter.cs
@@ -0,0 +1,16 @@
+﻿using ReportBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportBuilder
+{
+    public interface IReportConverter
+    {
+
+        public byte[] toByte(string reportFilePath, ReportExtension fileExtension);
+        public byte[] toByte(string reportFilePath, ReportExtension fileExtension, IDictionary<string, string> paramValues); 
+    }
+}
---
+++ Business/IReportReader.cs
@@ -0,0 +1,16 @@
+﻿using ReportBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportBuilder
+{
+    public interface IReportReader
+    {
+        public List<Parameter> Parameters();
+        public List<DataSource> DataSources();
+        public List<DataSet> DataSets();
+    }
+}
